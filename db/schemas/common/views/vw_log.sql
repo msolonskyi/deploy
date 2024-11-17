58731fe32debc12b3b2d8b2bb6cfa9d1f2b95896
msolonskyi <mykola.solonskyi@gmail.com>
2024-11-17 13:43:58 -0500

    Common user has been moved to misc repo.

--- db/schemas/common/views/vw_log.sql
+++
@@ -1,5 +0,0 @@
-create or replace view vw_log as
-select dtm, batch_id, text, clob_text, type, qty, id, start_dtm, finish_dtm, module, parameters, status, sid, serial#
-from batches b, log l
-where b.id = l.batch_id
-/
